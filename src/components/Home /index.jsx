934c560d9cd70d9a36bd1048c63e35acb5875adb
Isaac Vazquez <izzyinjurious@aim.com>
2021-05-06 17:04:26 -0500

    updated home page

---
+++ src/components/Home /index.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import {useForm} from "react-hook-form";
+import {yupResolver} from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+//stylesheets
+import './index.scss';
+//yup schema
+const emailSchema = yup
+    .object()
+    .shape({
+        email: yup
+            .string()
+            .required('Email is required'),
+    });
+
+
+const Home = (props) => {
+    const { register, handleSubmit, formState:{ errors } } = useForm({
+        resolver: yupResolver(emailSchema)
+      });
+    
+
+    const formFix = (formData) => {
+        const cleanData = {
+            'email': formData.email,
+            'attributes': {
+                email: formData.email,
+            }
+        }
+        return cleanData
+    }
+    const onSubmitEmail = (formData) => {
+        // formData.preventDefault()
+        const sendData = formFix(formData)
+        console.log(sendData);
+        let button = document.querySelector('.submit');
+       button.classList.add('clicked')
+       validate()
+       function validate() {
+        setTimeout(function () {
+         button.classList.remove("clicked");
+          button.classList.add("validate");
+          callback()
+        }, 2250);
+        }
+        function callback(){
+            setTimeout(function () {
+                    button.classList.remove("validate");
+            }, 1250);
+        }
+
+    }
+      
+    return <div className='billboard'>
+        <div className='container'>
+            <div className='row'>
+                <div className='billboard_textbox'>
+                    <h1>Coming<br/> Soon</h1>
+                    <p>
+                        Creating experiences for your event using our wristbands.
+                    </p>
+                    <form onSubmit={handleSubmit(onSubmitEmail)}>
+                        <input
+                            className='form-item'
+                            name="email"
+                            placeholder="Email"
+                            {...register("email",{pattern:"[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$"})} /> 
+                            {errors.email && <p>{errors.email.message}</p>}
+                        <input className='submit validate' type="submit" value=""/>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+};
+
+export default Home;
